649e3a2333f5a1dc5b356387d04de2bccb5d236c
QuickResolve <lauranmontoya@gmail.com>
2016-01-21 16:13:25 -0800

    update week 4 folders

--- week-4/triangle-side/my_solution.rb
+++ week-4/triangle-side/my_solution.rb
@@ -5,13 +5,18 @@
 # Your Solution Below
 
 def valid_triangle?(a, b, c)
-  # Your code goes here
+   # Your code goes here
   if (a + b) > c && c > (a-b).abs
+    return true
+  elsif (c + b) > a && a > (c-b).abs
+    return true
+  elsif (a + c) > b && b > (a-c).abs
+
     return true
   else
     return false
   end
-
 end
+
 
 valid_triangle?(10,15,10)
