3cb07379dda50085615509221de3674886897244
QuickResolve <lauranmontoya@gmail.com>
2016-02-06 16:01:58 -0800

    updated credit card

--- week-6/credit-card/my_solution.rb
+++ week-6/credit-card/my_solution.rb
@@ -25,77 +25,120 @@
 # of exactly 16 digits
 
 
-class CreditCard
+# class CreditCard
 
-def initialize(credit_card)
-  @credit_card = credit_card
+# def initialize(credit_card)
+#   @credit_card = credit_card
 
-  unless @credit_card.to_s.length == 16
-    raise ArgumentError.new("Invalid credit card.")
-  end
-end
+#   unless @credit_card.to_s.length == 16
+#     raise ArgumentError.new("Invalid credit card.")
+#   end
+# end
 
-def check_card
-  nums = @credit_card.to_s.split('').reverse
-
-  nums.each_with_index do |num,index|
-    if index %2 == 0
-      nums << num
-    elsif index %2 == 1
-      nums << (num.to_i*2).to_s
-    end
-
-  num_sum = 0
-
-  nums.split('').to_i.each do |x|
-    num_sum += x
-  end
-
-  if num_sum %10 == 0
-    true
-  else
-    false
-  end
-
- end
-
-end
-
- end
+# def check_card
+#   nums = @credit_card.to_s.split('').reverse
 
 
+#   nums.each_with_index do |num,index|
+#     if index %2 == 0
+#       nums << num
+#     elsif index %2 == 1
+#       nums << (num.to_i*2).to_s
+#     end
 
+
+#   num_sum = 0
+
+#   nums.to_s.split('').each do |x|
+#     num_sum += x.to_i
+#   end
+
+#   if num_sum %10 == 0
+#     true
+#   else
+#     false
+#   end
+
+#  end
+
+# end
+
+#  end
+
+
+# card = CreditCard.new(4408041234567901)
+# card.check_card
 
 
 # Refactored Solution
 
+# class CreditCard
+
+# def initialize(credit_card)
+#   @credit_card = credit_card
+
+#   unless @credit_card.to_s.length == 16
+#     raise ArgumentError.new("Invalid credit card.")
+#   end
+# end
+
+# def check_card
+#   nums = @credit_card.to_s.split('').reverse
+
+#   nums.each_with_index do |num,i|
+#     nums << num if i %2 == 0
+
+#     nums << (num.to_i*2).to_s if i %2 == 1
+
+#   num_sum = 0
+
+#   nums.to_s.split('').inject(0) {|num_sum, i|  num_sum + i.to_i }
+
+
+#   num_sum % 10 == 0 ? true : false
+#   end
+# end
+# end
+
+# card = CreditCard.new(4408041234567901)
+# card.check_card
+
+
+
 class CreditCard
+  def initialize(numbers)
+   unless numbers.to_s.length == 16
+     raise ArgumentError.new("The input has to be 16 digits long!")
+     end
+     @number = number
+   end
 
-def initialize(credit_card)
-  @credit_card = credit_card
-
-  unless @credit_card.to_s.length == 16
-    raise ArgumentError.new("Invalid credit card.")
+   def check_card
+     cc_number = @number.to_s.split('')
+     cc_number.map! { |num|
+       num.to_i
+     }
+     cc_number = doubling(cc_number)
+     final = cc_number.reduce(:+)
+     final % 10 == 0 ? true : false
   end
+
+   def doubling(array)
+     count = -2
+     while count >= -16
+       double = array[count] * 2
+      if double > 9
+         double = (double % 10) + 1
+       end
+       array[count] = double
+        count -= 2
+     end
+     return array
+   end
 end
 
-def check_card
-  nums = @credit_card.to_s.split('').reverse
-
-  nums.each_with_index do |num,i|
-    nums << num if i %2 == 0
-
-    nums << (num.to_i*2).to_s if i %2 == 1
-
-  num_sum = 0
-
-  nums.split('').to_i.inject(0) {|num_sum, i|  num_sum + i }
 
 
-  num_sum % 10 == 0 ? true : false
-  end
-end
-end
 
 
 # Reflection
