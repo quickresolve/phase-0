ef72cb26734d1b5dad7a1dc7d62b242e6b2cde02
QuickResolve <lauranmontoya@gmail.com>
2016-02-07 20:32:12 -0800

    update refactoring & reflections

--- week-5/die-class/my_solution.rb
+++ week-5/die-class/my_solution.rb
@@ -35,6 +35,20 @@ end
 
 # 3. Refactored Solution
 
+class Die
+  def initialize(sides)
+    @sides = sides
+
+    raise ArgumentError if sides < 1
+
+  end
+
+  attr_reader :sides
+
+  def roll
+   1+rand(@sides)
+  end
+end
 
 
 
@@ -42,3 +56,32 @@ end
 
 
 # 4. Reflection
+
+=begin
+
+What is an ArgumentError and why would you use one?
+
+You use an ArgumentError when the arguments are wrong and there isn’t a more specific Exception class. For example if the wrong number of integers or characters are entered, then you would return an argument error.
+
+What new Ruby methods did you implement? What challenges and successes did you have in implementing them?
+
+In this challenge I created a class, initialized it, and added methods to return specific values based on the input.
+
+What is a Ruby class?
+
+A class is the blueprint from which individual objects are created. When a new class is created, an object of type Class is initialized and assigned to a global constant.
+
+Why would you use a Ruby class?
+
+You would use a Ruby class to help you organize methods to be used in the future.
+
+What is the difference between a local variable and an instance variable?
+
+A local variable is only available locally inside of a specific method whereas an instance variable corresponds to a specific class.
+
+Where can an instance variable be used?
+
+Instance variables can be used in any method in the program as long as you call it with an @ symbol.
+
+
+=end
